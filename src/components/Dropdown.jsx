ad0a97dd68221c5d194402d96c4fcd88535789d3
CIBA <sanchezmariabelen01@gmail.com>
2024-09-07 20:31:06 +0200

    add path,useApi,App

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import UseApi from "../services/useApi"; // Asegúrate de ajustar la ruta correcta
+import { SERVICES } from "../config/urls";
+
+
 
 export default function DropdownComponent() {
-    const items = [
-        {
-            key: "servicios",
-            label: "Servicios",
-        },
-        {
-            key: "calendary",
-            label: "Calendario",
-        }
-    ];
+    // Estado para los servicios
+    const { data: services, loading, error } = UseApi({ apiEndpoint: SERVICES });
 
+    // Si está cargando, muestra un mensaje
+    if (loading) {
+        return <div>Cargando servicios...</div>;
+    }
+
+    // Si hay un error, muestra el error
+    if (error) {
+        return <div>Error al cargar los servicios: {error}</div>;
+    }
+
+    // Renderiza el dropdown con los servicios
     return (
         <Dropdown>
             <DropdownTrigger>
                 <Button
                     variant="faded"
+                    size="lg" // Cambia el tamaño del botón
+                    css={{ fontSize: "18px" }} // Aumenta el tamaño del texto en el botón
                 >
                     Servicios
                 </Button>
             </DropdownTrigger>
-            <DropdownMenu aria-label="Dynamic Actions" items={items}>
-                {(item) => (
+            <DropdownMenu aria-label="Dynamic Services" items={services}>
+                {(service) => (
                     <DropdownItem
-                        key={item.key}
-                        color={item.key === "delete" ? "danger" : "default"}
-                        className={item.key === "delete" ? "text-danger" : ""}
+                        key={service.id}
+                        className="service-item"
+                        css={{ fontSize: "16px" }} // Cambia el tamaño del texto en los items
                     >
-                        {item.label}
+                        {service.service_name} {/* Asume que el servicio tiene un campo "name" */}
                     </DropdownItem>
                 )}
             </DropdownMenu>
